551b3ca401030e9b63f6885b774b89364f5318ea
Jean-Elie Barjonet <je.barjonet@gmail.com>
2015-11-02 10:43:19 +0100

    app capable

--- client/global/router/seo.js
+++ client/global/router/seo.js
@@ -2,7 +2,14 @@ SEO.config({
     title: "Messtery",
     meta: {
         viewport: "width=device-width, initial-scale=1, maximum-scale=1",
-        language: "FR"
+        language: "EN",
+        'mobile-web-app-capable': 'yes',
+        'apple-mobile-web-app-capable': 'yes'
+    },
+    link: {
+        manifest: 'manifest.json',
+        icon: 'img/icons/favicon.png',
+        'apple-touch-icon': 'img/icons/favicon.png'
     },
     og: {
         title: "Messtery",
